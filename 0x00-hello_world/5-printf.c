a3a405c38c9d7989b8e2a5fd5384b13460963c86
Peace Okwara <Sweetieepie/alx-zero_day.git>
2023-03-16 15:47:56 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
+/*ii*
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: Always return 0 (Success)
  */
 int main (void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("\"with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
